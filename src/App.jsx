64b80b1e4b16374154b7322b7ae9aeb03aeae9da
Anshuman Tiwari <tanshuman145@gmail.com>
2024-10-06 03:11:27 +0530

    login ui only added

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,99 @@
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import { Home } from './components/Home';
-import {Meeting} from './components/Meeting';
-import { ScreenShare } from './components/ScreenShare';
-import { MessageBot } from './components/MessageBot';
-import MyEditor from './components/CodeEditor';
-import { Toaster } from "react-hot-toast"
-import YoutubeVideos from './components/YoutubeVideos';
-import YoutubeContent from './components/YoutubeContent';
-import MonacoCodeEditor from './components/MonacoCodeEditor';
-import Interview from './components/Interview';
-import ZoomSdk from './components/ZoomSdk';
-import WatchYoutube from './components/WatchYoutube';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Meeting } from "./components/Meeting";
+import { ScreenShare } from "./components/ScreenShare";
+import { MessageBot } from "./components/MessageBot";
+import MyEditor from "./components/CodeEditor";
+import { Toaster } from "react-hot-toast";
+import YoutubeVideos from "./components/YoutubeVideos";
+import YoutubeContent from "./components/YoutubeContent";
+import MonacoCodeEditor from "./components/MonacoCodeEditor";
+import Interview from "./components/Interview";
+import ZoomSdk from "./components/ZoomSdk";
+import WatchYoutube from "./components/WatchYoutube";
+import Login from "./components/Login";
 // import './index.css'
 function App() {
   const router = createBrowserRouter([
     {
-      path: '*',
-      element: <Home />
+      path: "*",
+      element: <Home />,
     },
     {
-      path: '/meeting/:meetingId',
-      element: <Meeting />
+      path: "/meeting/:meetingId",
+      element: <Meeting />,
     },
     {
-      path: '/screen',
-      element: <ScreenShare />
+      path: "/screen",
+      element: <ScreenShare />,
     },
     {
-      path:'/chat',
-      element: <MessageBot />
+      path: "/chat",
+      element: <MessageBot />,
     },
     {
-      path:'/editor',
-      element: <MonacoCodeEditor />
+      path: "/editor",
+      element: <MonacoCodeEditor />,
     },
     {
-      path:'/yt',
-      element: <YoutubeVideos />
+      path: "/yt",
+      element: <YoutubeVideos />,
     },
     {
-      path:'/ytContent',
-      element: <WatchYoutube />
+      path: "/ytContent",
+      element: <WatchYoutube />,
     },
     {
-      path:'/code/:meetingId',
-      element: <Interview />
+      path: "/code/:meetingId",
+      element: <Interview />,
     },
     {
-      path:'/zoom/call',
-      element: <ZoomSdk />
-    }
+      path: "/zoom/call",
+      element: <ZoomSdk />,
+    },
+    {
+      path: "/login",
+      element: <Login></Login>,
+    },
   ]);
 
   return (
     <>
       <RouterProvider router={router} />
       <Toaster
-          position="top-right"
-          reverseOrder={false}
-          toastOptions={{
-            success: {
-              style: {
-                backgroundColor: "white",
-                color: "green",
-                border: "1px solid green",
-                padding: "15px",
-                marginRight: "20px",
-              },
-              iconTheme: {
-                primary: "green",
-                secondary: "white",
-              },
+        position="top-right"
+        reverseOrder={false}
+        toastOptions={{
+          success: {
+            style: {
+              backgroundColor: "white",
+              color: "green",
+              border: "1px solid green",
+              padding: "15px",
+              marginRight: "20px",
             },
-            error: {
-              style: {
-                backgroundColor: "white",
-                color: "red",
-                border: "1px solid red",
-                padding: "15px",
-                marginRight: "20px",
-              },
-              iconTheme: {
-                primary: "red",
-                secondary: "white",
-              },
+            iconTheme: {
+              primary: "green",
+              secondary: "white",
             },
-          }}
-        />
+          },
+          error: {
+            style: {
+              backgroundColor: "white",
+              color: "red",
+              border: "1px solid red",
+              padding: "15px",
+              marginRight: "20px",
+            },
+            iconTheme: {
+              primary: "red",
+              secondary: "white",
+            },
+          },
+        }}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handlelogin = () => {
+    navigate("/login");
+  };
+
   return (
-    <header className="bg-white shadow-lg top-0 sticky">
+    <header className="bg-white shadow-md top-0 sticky">
       <div className="container mx-auto flex flex-wrap p-5 justify-between items-center">
         <a
           href="/"
@@ -36,6 +43,23 @@ const Navbar = () => {
             About Us
           </a>
         </nav>
+        <button
+          class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 cursor-pointer"
+          onClick={handlelogin}
+        >
+          Login
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
       </div>
     </header>
   );
